5be5b1cca0743acb51994410be26586d1e9d003d
atpfreelancer <atpfreelancer@gmail.com>
2023-05-15 23:17:38 +0300

    update

--- app.py
+++ app.py
@@ -12,12 +12,22 @@ openai_api_key = os.environ["OPENAIAPIKEY"]
 
 from review_end_point import endpoint
 from routes.routes import route
+from response_generator import generate_single_response, generate_multiple_responses
 
 app.register_blueprint(endpoint)
 app.register_blueprint(route)
 app.route('/convert_to_curl', methods=['POST'])(convert_to_curl)
 app.route('/explainapi', methods=['POST'])(generate_response)
 
+app.route('/single', methods=['POST'])
+def single():
+    return generate_single_response()
+
+app.route('/multiple', methods=['POST'])
+def multiple():
+    return generate_multiple_responses()
+
+
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8001, debug=True)
--- response_generator.py
+++ response_generator.py
@@ -1,50 +1,9 @@
-import os
+from flask import request, jsonify
 import openai
-from flask import Flask, request, jsonify
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-openai.api_key = os.environ["OPENAIAPIKEY"]
-app = Flask(__name__)
 
 # Global response variable
 response = None
 
-# def generate_response():
-#     data = request.get_json()
-
-#     try:
-#         prompt = data['prompt']
-#         list_data = data['data']
-#         if not isinstance(list_data, list):
-#             return jsonify({"error": "Expected a list of maps"}), 400
-
-#     except KeyError as e:
-#         return jsonify({"error": f"Missing key: {e.args[0]}"}), 400
-
-
-#     response = openai.Completion.create(
-#         engine="text-davinci-003",  # Code generation model
-#         prompt=prompt,
-#         max_tokens=150,
-#         temperature=0.7,
-#         user="my-unique-identifier"
-#     )
-
-
-
-#     return jsonify({"response": response.choices[0].text.strip()})
-
-
-
-app = Flask(__name__)
-
-# Global response variable
-response = None
-
-@app.route('/single', methods=['POST'])
 def generate_single_response():
     global response
     data = request.get_json()
@@ -63,7 +22,6 @@ def generate_single_response():
 
     return jsonify({"response": response.choices[0].text.strip()})
 
-@app.route('/multiple', methods=['POST'])
 def generate_multiple_responses():
     global response
     data = request.get_json()
@@ -81,13 +39,10 @@ def generate_multiple_responses():
             return jsonify({"error": f"Missing key in item: {e.args[0]}"}), 400
 
         # Use the single response function
-        single_response = generate_single_response(prompt)
+        single_response = generate_single_response()
 
         # Append the response text to the responses list
         responses.append({"api_name": api_name, "response": single_response})
 
     # Return a JSON object with all the responses
     return jsonify(responses)
-
-if __name__ == '__main__':
-    app.run(debug=True)
