cc9792aa99e88baff0387eabbc2cef41ba5445d9
atpfreelancer <atpfreelancer@gmail.com>
2023-05-24 13:33:07 +0300

    update

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import MySQLdb
 
-connection = MySQLdb.connect(
-    user='atpfreelancer',
-    passwd='atp5797895',
-    host='atpfreelancer.mysql.pythonanywhere-services.com',
-    db='atpfreelancer$eva',
-)
+try:
+    connection = MySQLdb.connect(
+        user='atpfreelancer',
+        passwd='atp5797895',
+        host='atpfreelancer.mysql.pythonanywhere-services.com',
+        db='atpfreelancer$eva',
+    )
 
-# Do stuff
+    cursor = connection.cursor()
+    cursor.execute("SELECT 1")  # Run a simple query
+    result = cursor.fetchone()
+    if result is not None:
+        print("Connected to the database")
+    else:
+        print("Failed to connect to the database")
+    
+    # Do stuff
 
-connection.close()
+finally:
+    connection.close()
