8af94d9f00baca8822770ccc2a4c23dbbb5a8721
atpfreelancer <atpfreelancer@gmail.com>
2023-05-24 13:30:22 +0300

    update

--- test.py
+++ test.py
@@ -1,19 +1,12 @@
 import MySQLdb
-import sshtunnel
 
-sshtunnel.SSH_TIMEOUT = 5.0
-sshtunnel.TUNNEL_TIMEOUT = 5.0
+connection = MySQLdb.connect(
+    user='atpfreelancer',
+    passwd='atp5797895',
+    host='atpfreelancer.mysql.pythonanywhere-services.com',
+    db='atpfreelancer$eva',
+)
 
-with sshtunnel.SSHTunnelForwarder(
-    ('ssh.pythonanywhere.com'),
-    ssh_username='atpfreelancer@gmail.com', ssh_password='atp5797895',
-    remote_bind_address=('atpfreelancer.mysql.pythonanywhere-services.com', 3306)
-) as tunnel:
-    connection = MySQLdb.connect(
-        user='atpfreelancer',
-        passwd='atp5797895',
-        host='127.0.0.1', port=tunnel.local_bind_port,
-        db='atpfreelancer$eva',
-    )
-    # Do stuff
-    connection.close()
+# Do stuff
+
+connection.close()
