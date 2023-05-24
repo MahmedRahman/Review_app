557b22fb5a8f60f7ffaf0a1b885aa01c989a92a0
atpfreelancer <atpfreelancer@gmail.com>
2023-05-24 11:59:19 +0300

    update

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from user_table import UserTable
 
 from back.app_review.review_end_point import endpoint_app_review
 from back.app_postman.postman_end_point import endpoint_postman_app
-from user_table_end_point import endpoint_user_table
 
 app = Flask(__name__)
 
@@ -18,7 +17,6 @@ app.register_blueprint(route)
 
 app.register_blueprint(endpoint_app_review)
 app.register_blueprint(endpoint_postman_app)
-app.register_blueprint(endpoint_user_table)
 
 
 
