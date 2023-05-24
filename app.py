3df184cf07ba251fcb6792254d44ca18c985cf01
atpfreelancer <atpfreelancer@gmail.com>
2023-05-24 13:51:52 +0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from routes.routes import route
 from back.app_review.review_end_point import endpoint_app_review
 from back.app_postman.postman_end_point import endpoint_postman_app
-from back.user_table.user_table_end_point import endpoint_user_tabel_app
+from back.user_table.user_table_end_point import endpoint_user_table
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ openai_api_key = os.environ["OPENAIAPIKEY"]
 app.register_blueprint(route)
 app.register_blueprint(endpoint_app_review)
 app.register_blueprint(endpoint_postman_app)
-app.register_blueprint(endpoint_user_tabel_app)
+app.register_blueprint(endpoint_user_table)
 
 
 
